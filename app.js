c35d46ebbc1a06057d2740ec8264c9e6faf43df2
YiChao <frank22552000@gmail.com>
2019-11-20 20:16:58 +0800

    modify app.js

--- app.js
+++ app.js
@@ -52,75 +52,12 @@ app.get('/search', (req, res) => {
   })
 
 })
-// //列出所有 restaurants
-// app.get('/', (req, res) => {
-//   RestaurantList.find((err, restaurants) => {
-//     if (err) return console.error(err)
-//     return res.render('index', { restaurants: restaurants })
-//   })
-// })
+
 
 app.use('/', require('./routes/home'))
 app.use('/restaurants', require('./routes/restaurantList'))
 
-// // 新增一筆 restaurants 頁面
-// app.get('/new', (req, res) => {
-//   return res.render('new')
-// })
-// // 顯示一筆 restaurants 的詳細內容
-// app.get('/restaurants/:id', (req, res) => {
-//   RestaurantList.findById(req.params.id, (err, restaurant) => {
-//     if (err) return console.error(err)
-//     return res.render('show', { restaurant: restaurant })
-//   })
-// })
-// // 修改 restaurants 頁面
-// app.get('/restaurants/:id/edit', (req, res) => {
-//   RestaurantList.findById(req.params.id, (err, restaurant) => {
-//     if (err) return console.error(err)
-//     return res.render('edit', { restaurant: restaurant })
-//   })
-// })
-// // 新增一筆 RestaurantList
-// app.post('/new', (req, res) => {
-//   const restaurant = new RestaurantList({
-//     name: req.body.name,  // name 是從 new 頁面 form 傳過來
-//     name_en: req.body.name_en,
-//     category: req.body.category,
-//     image: req.body.image,
-//     location: req.body.location,
-//     phone: req.body.phone,
-//     google_map: req.body.google_map,
-//     rating: req.body.rating,
-//     description: req.body.description
-//   })
-//   restaurant.save(err => {
-//     if (err) return console.error(err)
-//     return res.redirect('/')  // 新增完成後，將使用者導回首頁
-//   })
-// })
-// // 修改 RestaurantList
-// app.put('/restaurants/:id/edit', (req, res) => {
-//   RestaurantList.findById(req.params.id, (err, restaurantList) => {
-//     if (err) return console.error(err)
-//     restaurantList.name = req.body.name
-//     restaurantList.save(err => {
-//       if (err) return console.error(err)
-//       return res.redirect(`/restaurants/${req.params.id}`)
-//     })
-//   })
-// })
 
-// //刪除 restaurants
-// app.delete('/restaurants/:id/delete', (req, res) => {
-//   RestaurantList.findById(req.params.id, (err, restaurant) => {
-//     if (err) return console.error(err)
-//     restaurant.remove(err => {
-//       if (err) return console.error(err)
-//       return res.redirect('/')
-//     })
-//   })
-// })
 
 // start and listen on the Express server
 app.listen(port, () => {
