08fbb8a8fc4f464152aaae04f9fb5031c53a491a
YiChao <frank22552000@gmail.com>
2020-08-23 22:24:40 +0800

    edit Procfile

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Todo = require('../restaurantList')
 const restaurantList = require('../../restaurant.json')
 
-mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 
 db.on('error', () => {
